faeb0a61a7f90959b397a09cc94551b27bd052b1
Vu Duc Tai <tai@inspectorio.com>
2019-10-27 19:57:21 +0700

    fix setup

--- setup.py
+++ setup.py
@@ -11,12 +11,7 @@ setup(
     author="louis",
     author_email="vuductaiptit@gmail.com",
     description="Wizard for final project at school",
-    long_description_content_type="text/markdown",
     url="https://github.com/warrior-pinpoint/ptit-wizard",
     packages=find_packages(),
     install_requires=requirements,
-    classifiers=[
-        "Programming Language :: Python :: 3.6",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-    ],
 )
